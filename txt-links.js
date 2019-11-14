57ddef3aca380261ab8b933b6b48539f76c55a1f
Fulvio <fulviocanducci@hotmail.com>
2019-11-14 15:15:59 -0300

    Second Commit

---
+++ txt-links.js
@@ -0,0 +1,5 @@
+https://code.visualstudio.com/docs/editor/userdefinedsnippets
+https://code.visualstudio.com/docs/editor/userdefinedsnippets
+https://code.visualstudio.com/docs/editor/userdefinedsnippets#_grammar
+
+https://github.com/xabikos/vscode-javascript
