6dafe84861829c0ba41b703fdecc76b76eeec3bb
Fulvio <fulviocanducci@hotmail.com>
2019-11-14 21:43:12 -0300

    Third Commit

--- txt-links.js
+++ txt-links.js
@@ -1,5 +1,5 @@
 https://code.visualstudio.com/docs/editor/userdefinedsnippets
-https://code.visualstudio.com/docs/editor/userdefinedsnippets
+https://www.lambda3.com.br/2016/04/criando-extensoes-para-o-visual-studio-code/
 https://code.visualstudio.com/docs/editor/userdefinedsnippets#_grammar
 
 https://github.com/xabikos/vscode-javascript
